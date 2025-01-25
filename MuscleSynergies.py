fdf0ef68e8b166f481c06e5ad3b01c18e3a1688b
charles <go25qul@mytum.de>
2025-01-25 21:00:03 +0100

    do NMF and plot r2

--- MuscleSynergies.py
+++ MuscleSynergies.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import wfdb
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import NMF
 
 def plot_emg_channel(emg_signal, time, channel_idx=0, save=False, file_name = None):
     """
@@ -94,15 +95,93 @@ def Principle_Component_Analysis(data, num_components=4):
     data_reconstructed : 2d numpy array in shape (n_samples, n_channels)
         Reconstructed data after PCA
     """
-    scaler = StandardScaler()
-    data_std = scaler.fit_transform(data)
+    # scaler = StandardScaler()
+    # data_std = scaler.fit_transform(data)
 
     pca = PCA(num_components)
-    data_pca = pca.fit_transform(data_std)
+    data_pca = pca.fit_transform(data)
 
     data_reconstructed = pca.inverse_transform(data_pca)
 
     return data_pca, data_reconstructed
+
+def Non_negative_Matrix_Factorization(data, num_components=4):
+    """
+    Perform Non-negative Matrix Factorization (NMF) on the EMG signal
+
+    Parameters
+    ----------
+    data : 2d numpy array in shape (n_samples, n_channels)  
+        EMG signal
+    num_components : int
+        Number of components to extract
+
+    Returns
+    -------
+    W : 2d numpy array in shape (n_samples, num_components)
+        weights of respective channels
+    H : 2d numpy array in shape (num_components, n_channels)
+        Dynamic changes in synergy
+    data_reconstructed : 2d numpy array in shape (n_samples, n_channels)
+        Reconstructed data after NMF
+    """
+    # scaler = MinMaxScaler()
+    # data_std = scaler.fit_transform(data)
+    min_value = np.min(data)
+    if min_value < 0:
+        data_shifted = data - min_value
+    else:
+        data_shifted = data
+
+    nmf = NMF(num_components, max_iter=100000, init='random', random_state=42)
+    W = nmf.fit_transform(data_shifted)
+
+    H = nmf.components_
+
+    data_reconstructed = np.dot(W, H) + min_value
+
+    return W, H, data_reconstructed
+
+def Run_NMF_different_components(data, components=[]):
+    """
+    Perform Non-negative Matrix Factorization (NMF) on the EMG signal with different number of components
+    
+    Parameters
+    ----------
+    data : 2d numpy array in shape (n_samples, n_channels)  
+        EMG signal
+    components : list
+        List of number of components to extract
+
+    Returns
+    -------
+    W : list of 2d numpy array in shape (n_samples, num_components)
+        weights of respective channels
+    H : list of 2d numpy array in shape (num_components, n_channels)
+        Dynamic changes in synergy
+    reconstructions : list of 2d numpy array in shape (n_samples, n_channels)    
+        Reconstructed data after NMF
+    r2_NMF : 2d numpy array in shape (len(components), n_channels)
+        R2 values for each channel with different number of components
+    r2_NMF_mean : 1d numpy array in shape (len(components))
+        Mean R2 values for each number of components
+    """
+    
+    r2_NMF = np.zeros((len(components), emg_signal.shape[1]))
+    r2_NMF_mean = np.zeros(len(components))
+    W, H, reconstructions = [], [], []
+
+    for i in components:
+        print (f'Running NMF with {i} components')
+        W_i, H_i, reconstructed = Non_negative_Matrix_Factorization(data, i)
+        W.append(W_i)
+        H.append(H_i)
+        reconstructions.append(reconstructed)
+        r2_NMF[i - 1, :] = compute_r2(emg_signal, reconstructed)
+        r2_NMF_mean[i - 1] = np.mean(r2_NMF[i - 1, :])
+        print(f'R2_NMF mean for {i} components: {r2_NMF_mean[i - 1]:.4f}')
+
+    return W, H, reconstructions, r2_NMF, r2_NMF_mean
 
 def compute_r2(original, reconstructed):
     correlation_matrix = np.corrcoef(original.T, reconstructed.T)
@@ -126,15 +205,67 @@ def plot_emg_components(time, num_components, pca, save=False, file_name = None)
         plt.savefig(file_name)
     plt.show()
 
-def plot_origin_vs_reconstructed(time, original, reconstructed, save=False, file_name = None):
+def plot_origin_vs_reconstructed(time, original, reconstructed_pca = None,  reconstructed_nmf = None, save=False, file_name = None):
     plt.figure(figsize=(10, 6))
     plt.suptitle("Original vs Reconstructed EMG signal", fontsize=16)
-    plt.plot(time[0:500], original[0:500], label = "Original EMG signal", color = 'b')
-    plt.plot(time[0:500], reconstructed[0:500], label = "Reconstructed EMG signal", color = 'r')
+    plt.plot(time[0:500], original[0:500], label = "Original EMG signal", color = '#1f77b4')
+    if reconstructed_pca is not None:
+        plt.plot(time[0:500], reconstructed_pca[0:500], label = "PCA Reconstructed EMG signal", color = '#ff7f0e')
+    if reconstructed_nmf is not None:
+        plt.plot(time[0:500], reconstructed_nmf[0:500], label = "NMF Reconstructed EMG signal", color = '#2baf6a')
     plt.xlabel('Time (s)')
     plt.ylabel('Amplitude')
     plt.legend(loc='upper right')
     
+    if save:
+        plt.savefig(file_name)
+    plt.show()
+
+def plot_W(W, save=False, file_name = None):
+    plt.figure(figsize=(16, 9))
+    plt.suptitle("Weight of channels", fontsize=16)
+    for i in range (W.shape[1]):
+        plt.subplot(W.shape[1],1,i+1)
+        plt.plot(W[:, i], label = f"Component #{i+1}", color = plt.cm.viridis(i/W.shape[1]))
+        plt.title(f"Component {i+1}")
+        plt.xlabel('Time (s)')
+        plt.ylabel('Amplitude')
+        plt.legend(loc='upper right')
+
+    plt.tight_layout()
+    if save:
+        plt.savefig(file_name)
+    plt.show()
+
+def plot_H(H, save=False, file_name = None):
+    plt.figure(figsize=(16, 9))
+    plt.suptitle("Dynamic Changes in Synergy", fontsize=16)
+    for i in range (H.shape[0]):
+        plt.subplot(H.shape[0],1,i+1)
+        plt.plot(H[i, :], label = f"Component #{i+1}", color = plt.cm.viridis(i/H.shape[0]))
+        plt.title(f"Component {i+1}")
+        plt.xlabel('Time (s)')
+        plt.ylabel('Amplitude')
+        plt.legend(loc='upper right')
+
+    plt.tight_layout()
+    if save:
+        plt.savefig(file_name)
+    plt.show()
+
+def plot_r2_nmf_values(r2_NMF_mean, components, save=False, file_name = None):
+    plt.figure(figsize=(12, 6))
+    bars = plt.bar(components, r2_NMF_mean)
+    plt.title('R2 values of NMF with different number of components')
+    plt.xlabel('Number of components')
+    plt.ylabel('R2 value')
+    plt.xticks(components)
+    plt.grid(axis='y')
+
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width()/2, yval, f'{yval:.3f}', ha='center', va='bottom')
+
     if save:
         plt.savefig(file_name)
     plt.show()
@@ -166,17 +297,38 @@ if __name__ == "__main__":
 
     #Principle Component Analysis
     num_components = 4
-    emg_pca, emg_reconstructed= Principle_Component_Analysis(emg_signal, num_components)
+    emg_pca, emg_reconstructed_PCA= Principle_Component_Analysis(emg_signal, num_components)
     # plot components of EMG signal
     # plot_emg_components(time, num_components, emg_pca, save = False, file_name = 'emg_components.png')
 
-    r2 = compute_r2(emg_signal, emg_reconstructed)
-    print("R2.shape: ", r2.shape)
+    r2_PCA = compute_r2(emg_signal, emg_reconstructed_PCA)
+    r2_PCA_mean = np.mean(r2_PCA)
+    print(f'R2_PCA mean: {r2_PCA_mean}')
     #plot first channel of original and reconstructed EMG signal
-    plot_origin_vs_reconstructed(time, emg_signal_raw[:, 0], emg_reconstructed[:, 0], save=False, file_name = 'origin_vs_reconstructed.png')
+    # plot_origin_vs_reconstructed(time, emg_signal[:, 0], emg_reconstructed_PCA[:, 0], save=False, file_name = 'origin_vs_PCA_reconstructed.png')
 
-    
+    #Non-negative Matrix Factorization
+    num_components = 4
+    W, H, emg_reconstructed_NMF = Non_negative_Matrix_Factorization(emg_signal, num_components)
+    # plot components of EMG signal
+    # plot_W(W, save = False, file_name = 'W.png')
+    # plot_H(H, save = False, file_name = 'H.png')
 
+    #plot first channel of original and reconstructed EMG signal
+    # plot_origin_vs_reconstructed(time, emg_signal[:, 0], reconstructed_pca= None, reconstructed_nmf = emg_reconstructed_NMF[:, 0],save=False, file_name = 'origin_vs_NMF_reconstructed.png')
+    r2_NMF = compute_r2(emg_signal, emg_reconstructed_NMF)
+    r2_NMF_mean = np.mean(r2_NMF)
+    print(f'R2_NMF mean: {r2_NMF_mean}')
+
+    #plot PCA and NMF comparison
+    # plot_origin_vs_reconstructed(time, emg_signal[:, 0], emg_reconstructed_PCA[:, 0], emg_reconstructed_NMF[:, 0], save=False, file_name = 'origin_vs_PCA_NMF_reconstructed.png')
+
+    #Use NMF to extract muscle synergies 1 to 15 components
+    components = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    W, H, reconstructions, r2_NMF, r2_NMF_mean = Run_NMF_different_components(emg_signal, components)
+
+    #plot R2 values
+    plot_r2_nmf_values(r2_NMF_mean, components, save=False, file_name = 'r2_NMF_values.png')
 
 
 
